ac942da4fc2c17c8643270069117a228a0658abc
yuting zhang <zhangyuting675@gmail.com>
2016-04-05 16:43:06 -0400

    2016-04-04 a2 is finish

--- a2.py
+++ a2.py
@@ -24,11 +24,14 @@ def tfInput(string):
     answer = input(string)
     return answer[0].lower() == 'y'
 
-
 # ICS4U:
 
-#1 transform string level to numerical percentage
+#1
 def level2Percent(level):
+        '''
+        1.Transform string level to numerical percentage.
+        '''
+        # codes for changing different level to percent
         if level=='4+':
             return 100
         elif level=='4':
@@ -56,8 +59,12 @@ def level2Percent(level):
         elif level=='< 1':
             return 40
 
-#2 transform numerical percentage to string level
+#2
 def percent2Level(percent):
+        '''
+        2.Transform numerical percentage to string level.
+        '''
+        # codes for changing different percentage to levels
         if percent in range (95,101):
             return "4+"
         elif percent in range (87,95):
@@ -85,74 +92,107 @@ def percent2Level(percent):
         elif percent in range (0,50):
             return "<1"
 
-#3 to check if two are the same
+#3
 def stringCompare(string1,string2):
+    '''
+    3.Be able to check if two strings are the same.
+    '''
+    # returns the equation of string compare, it used for making two different type but same letters'string same
     return string1.lower().strip()== string2.lower().strip()
 
-#4 be able to add new grades to database
+#4
 def addGrade(database,course,assignment,student,grade):
+    '''
+    4.Be able to add new grades to database.
+    '''
+    # returns the formula of adding database
     return database+[[course,assignment,student,grade]]
 
-#5 allows the user to input more grades into database
+#5
 def inputGrades(database):
+        '''
+        5. Allows the user to input more grades into database
+        '''
+        # Asking user question and save their input into database
         course=input('please enter the course name:')
         assignment=input('please enter the name of the assignment/test/exam:')
         student=input('please enter the student name:')
         grade=input('please enter the grade that student have:')
         database=database+[[course,assignment,student,grade]]
 
+        # looping and Asking if user wants to input another grade
         while True:
             decision=tfInput('would you like to input another grade?')
+            # ask user question if they decide to input one more grade
             if decision==True:
                 student=input('please enter the student name:')
                 grade=input('please enter the grade that student have:')
                 database=database+[[course,assignment,student,grade]]
-
+            # break the loop if user decide to not inputs anymore grades into database
             elif decision==False:
                 break
-
+    # return the database
         return database
 
-#6 be able to returns the average percent grade for the specified student.
+#6
 def studentAverage(database,student):
+    '''
+    6. Be able to returns the average percent grade for the specified student.
+    '''
+    # An array that can store grades in string type.
     gLevel=[]
+    # An array that can store grades in percentage type.
     gPercent=[]
+    # The total of grades.
     sum=0
 
+    # Functions that is able to find the right student and store grades in to the array I have created name gLevel.
     for e in range(len(database)):
         if stringCompare(database[e][2],student)== True:
             gLevel.append(database[e][3])
-
+    # Functions that allows you to change level to percent and store percentages in to the array name gPercent.
     for r in range(len(gLevel)):
         gPercent.append(level2Percent(gLevel[r]))
-
+    # Functions that can add grades together and became the Sum.
     for f in range(len(gPercent)):
         sum=(sum+gPercent[f])
-
+    # Return the formula of calculating the average
     return sum/len(gPercent)
 
-#7 be able to returns the average percent grade for specified course
+#7
 def courseAverage(database,course):
+    '''
+    7. Be able to returns the average percent grade for the specified course.
+    '''
+    # An array that can store grades in string type.
     gLevel=[]
+    # An array that can store grades in percentage type.
     gPercent=[]
+    # The total of grades.
     sum=0
 
+    # Functions that is able to find the right course and store grades in to the array I have created name gLevel.
     for e in range(len(database)):
         if stringCompare(database[e][0],course)== True:
             gLevel.append(database[e][3])
 
+    # Functions that allows you to change level to percent and store percentages in to the array name gPercent.
     for r in range(len(gLevel)):
         gPercent.append(level2Percent(gLevel[r]))
 
+    # Functions that can add grades together and became the Sum.
     for f in range(len(gPercent)):
         sum=(sum+gPercent[f])
 
+    # Return the formula of calculating the average
     return sum/len(gPercent)
 
+
+
+# codes that are able to let program testing/running
 if __name__=="__main__":
     pass
     # ICS4U:
-    # codes that are able to let program testing/running
     #1
     print(level2Percent('4+'))
     #2
