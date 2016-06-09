9643239112d3fad68855730928d899ab9d083b57
yuting zhang <zhangyuting675@gmail.com>
2016-06-09 16:13:13 -0400

    2016-05-01 a2 is finish

--- a5.py
+++ a5.py
@@ -1,39 +1,62 @@
 '''
 Program:    a5.py
-Name:       
-Date:       
-Desc:       
+Name:       John Zhang
+Date:       6/9/2016
+Desc:       assignment five
 '''
 
 import os
 
 ### PROGRAM YOUR FUNCTIONS HERE! ###
 def power(x,n):
+    '''
+    This function computes the power of x^n
+    '''
     if n==0:
+        #Return if that n=0
         return 1
+    # Return function
     return x*power(x,n-1)
 
 def morePower(x,n):
+    '''
+    This function computes the power of x^n, using the following rules
+    '''
     if n==0:
+        #Return if that n=0
         return 1
+    #Return if n is even
     if n%2==0:
         return power(x,n//2)*power(x,n//2)
+    #Return if n is odd
     return x*power(x,n//2)*power(x,n//2)
 
 def GCF(a,b):
+    '''
+    This function finds the greatest common factor of and two numbers. The greatest common factor is the largest number
+    that divides evenly into both numbers
+    '''
     if b==0:
+        #Return if that n=0
         return a
+    #Return the function
     return GCF(b,a%b)
 
 def isPalindrome(string):
+    '''
+    This function checks whether a string is palindrome -  a word that is the same forwards and backwards
+    '''
     if string=='':
+        # Return if there is nothing in the string
         return None
-    elif string[0]==string[-1]:
-        return isPalindrome(string[1:-2])
-    elif len(string)==0 or 1:
+    elif string[0]!=string[-1]:
+        # Return is the string are not same forwards and backwards
+        return False
+    elif len(string)<2:
+        # Return if the letters string are less than 2
         return True
-    return False
-
+    # Return the function
+    return isPalindrome(string[1:-1])
 
 #def printFiles(root):
 
@@ -44,4 +67,4 @@ if __name__=="__main__":
 print(power(2,7))
 print(morePower(5,4))
 print(GCF(12,8))
-print(isPalindrome('civic'))
+print(isPalindrome('civsdic'))
